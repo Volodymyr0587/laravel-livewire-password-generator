fb4efc50566e2a8a44be1323dc133d3c8bdaf4c3
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-04-02 16:17:21 +0300

    Add Footer; Add Current Year to Footer; Add Image

--- resources/views/livewire/password-generator.blade.php
+++ resources/views/livewire/password-generator.blade.php
@@ -46,10 +46,11 @@
     <div class="container mx-auto px-4">
         <div class="flex flex-col md:flex-row items-center justify-between">
             <div class="md:w-1/2 mb-8 md:mb-0">
-                <h1 class="text-white font-bold text-5xl leading-tight mb-6">Discover the world's best coffee</h1>
+                <h1 class="text-white font-bold text-5xl leading-tight mb-6">Quickly Generate Customized Secure Passwords</h1>
             </div>
             <div class="md:w-1/2">
-                <img src="https://plus.unsplash.com/premium_photo-1675716443562-b771d72a3da7" alt="Coffee beans"
+
+                <img src="https://images.unsplash.com/photo-1633265486064-086b219458ec?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Password lock"
                     class="w-full rounded-lg shadow-lg">
             </div>
         </div>
@@ -59,7 +60,10 @@
 <!-- Featured section -->
 <section class="py-20">
     <div class="container mx-auto px-4">
-        <h2 class="text-3xl font-bold text-gray-800 mb-8">Featured products</h2>
+        <h2 class="inline-flex text-3xl font-bold text-gray-800 mb-8">
+            Password generator
+            <svg class="w-5 h-5 ml-2" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M11 11H10V10H11V11Z" fill="#000000"></path> <path d="M8 11H9V10H8V11Z" fill="#000000"></path> <path d="M13 11H12V10H13V11Z" fill="#000000"></path> <path fill-rule="evenodd" clip-rule="evenodd" d="M3 6V3.5C3 1.567 4.567 0 6.5 0C8.433 0 10 1.567 10 3.5V6H11.5C12.3284 6 13 6.67157 13 7.5V8.05001C14.1411 8.28164 15 9.29052 15 10.5C15 11.7095 14.1411 12.7184 13 12.95V13.5C13 14.3284 12.3284 15 11.5 15H1.5C0.671573 15 0 14.3284 0 13.5V7.5C0 6.67157 0.671573 6 1.5 6H3ZM4 3.5C4 2.11929 5.11929 1 6.5 1C7.88071 1 9 2.11929 9 3.5V6H4V3.5ZM8.5 9C7.67157 9 7 9.67157 7 10.5C7 11.3284 7.67157 12 8.5 12H12.5C13.3284 12 14 11.3284 14 10.5C14 9.67157 13.3284 9 12.5 9H8.5Z" fill="#000000"></path> </g></svg>
+        </h2>
 
             <form wire:submit.prevent="generatePasswords" class="max-w-sm mx-auto mb-10">
                 @csrf
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,5 +11,12 @@
 <body>
     @livewire('password-generator')
     @livewireScripts
+
+    <footer class="bg-zinc-50 text-center dark:bg-neutral-700 lg:text-left">
+        <div class="bg-black/5 p-4 text-center text-surface dark:text-white">
+          © <?= date('Y'); ?> Copyright:
+          <a href="#">PG</a>
+        </div>
+      </footer>
 </body>
 </html>
